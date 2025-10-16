27abd11ea481cba0dfa7539b8f55927b61994279
mshafiqyajid <mshafiqyajid@gmail.com>
2025-10-16 10:49:13 +0800

    fix eslint issue

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,39 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
+const path = require('path');
 
 module.exports = defineConfig([
-  expoConfig,
+  ...expoConfig,
   {
     ignores: ['dist/*'],
   },
   {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    languageOptions: {
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
+      },
+    },
     settings: {
+      'import/parsers': {
+        '@typescript-eslint/parser': ['.ts', '.tsx', '.js', '.jsx'],
+      },
       'import/resolver': {
         typescript: {
           alwaysTryTypes: true,
-          project: './tsconfig.json',
+          project: path.resolve(__dirname, './tsconfig.json'),
         },
       },
+    },
+    rules: {
+      'import/no-unresolved': [
+        'error',
+        {
+          ignore: ['^@/'],
+        },
+      ],
     },
   },
 ]);
